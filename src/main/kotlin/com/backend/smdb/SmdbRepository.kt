f8979ead34eedc9fb2c0b894a5f538ca7d9c8943
Sondre Widmark <sondre.widmark@gmail.com>
2022-01-14 00:03:54 +0100

    Add new db queries

--- src/main/kotlin/com/backend/smdb/SmdbRepository.kt
+++ src/main/kotlin/com/backend/smdb/SmdbRepository.kt
@@ -1,5 +1,6 @@
 package com.backend.smdb
 
+import org.springframework.data.mongodb.repository.DeleteQuery
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.mongodb.repository.Query
 import org.springframework.stereotype.Repository
@@ -9,4 +10,10 @@ interface SmdbRepository : MongoRepository<Movie, String> {
 
     @Query("{ markedFavourite: true}")
     fun getFavourites(): List<Movie>
+
+    @Query("{ externalId : ?0 }")
+    fun getFavouriteMovie(externalId: Int): Movie?
+
+    @DeleteQuery("{ externalId = ?0 }")
+    fun deleteMovie(externalId: Int)
 }
