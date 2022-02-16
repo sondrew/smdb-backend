2e406710b1947b0c2807094417769206a3cb1309
Sondre Widmark <sondre.widmark@gmail.com>
2022-02-16 22:28:14 +0100

    Create response model when retrieving recommendation list

--- src/main/kotlin/com/backend/smdb/models/ResponseModels.kt
+++ src/main/kotlin/com/backend/smdb/models/ResponseModels.kt
@@ -1,7 +1,10 @@
 package com.backend.smdb.models
 
 import com.backend.smdb.MediaType
+import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.LastModifiedDate
+import java.util.*
+import javax.annotation.processing.Generated
 
 data class MovieResponseModel(
     // Internal data
@@ -25,7 +28,6 @@ data class MovieResponseModel(
     val releaseDate: String,
     val genres: List<String>
 )
-
 
 data class TMDbMovieResponseModel(
     val id: Int,
@@ -72,5 +74,30 @@ data class MovieRequestModel(
     val popularity: Double,
     val overview: String,
     val releaseDate: String,
+    val genres: List<String>
+)
+
+
+data class RecommendationListResponseModel(
+    // @Id @Generated val listId: UUID = UUID.randomUUID(),
+    val id: String,
+    val listName: String,
+    val listDescription: String?,
+    val list: List<RecommendedMediaResponseModel>
+)
+
+//@Document - maybe not needed when already inside RecommendationList?
+data class RecommendedMediaResponseModel (
+    val id: Int,
+    val listIndex: Int, // maybe index is not needed? could maintain list index inside object without this. unsure if it's stored 1-to-1 in sequence
+    //@Id val listId: String - should it have a unique ID in this list? or is it sufficient with index?
+    val userRating: Int?,
+    val userComment: String?,
+    val title: String,
+    val originalTitle: String?,
+    val description: String?,
+    val mediaType: MediaType,
+    val posterPath: String?, // get from backend
+    val imdbPath: String?, // backend
     val genres: List<String>
 )
