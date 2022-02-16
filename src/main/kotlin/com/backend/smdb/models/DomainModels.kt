d2dbc797b5a7f338cdf16a54b2815cfd4e7df6ed
Sondre Widmark <sondre.widmark@gmail.com>
2022-02-16 22:26:06 +0100

    Create domain and database models for recommendation lists

--- src/main/kotlin/com/backend/smdb/models/DomainModels.kt
+++ src/main/kotlin/com/backend/smdb/models/DomainModels.kt
@@ -1,9 +1,8 @@
 package com.backend.smdb
 
 import com.backend.smdb.models.MovieResponseModel
-import com.mongodb.annotations.Immutable
-import org.bson.types.ObjectId
-import org.springframework.data.annotation.CreatedDate
+import com.backend.smdb.models.RecommendationListResponseModel
+import com.backend.smdb.models.RecommendedMediaResponseModel
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.mongodb.core.mapping.Document
@@ -57,3 +56,51 @@ data class Movie(
             genres = genres
         )
 }
+
+@Document
+data class RecommendationList(
+    // @Id @Generated val listId: UUID = UUID.randomUUID(),
+    @Id @Generated val id: String = UUID.randomUUID().toString().replace("-","").substring(0,8),
+    val listName: String,
+    val listDescription: String?,
+    val list: List<RecommendedMedia>,
+    val adminId: UUID = UUID.randomUUID() // to not pass to frontend model
+){
+    fun toResponseModel(): RecommendationListResponseModel =
+        RecommendationListResponseModel(
+            id,
+            listName,
+            listDescription,
+            list.map { RecommendedMediaResponseModel(
+                it.id,
+                it.listIndex,
+                it.userRating,
+                it.userComment,
+                it.title,
+                it.originalTitle,
+                it.description,
+                it.mediaType,
+                it.posterPath,
+                it.imdbPath,
+                it.genres
+            ) }.sortedBy { it.listIndex }
+        )
+}
+
+//@Document - maybe not needed when already inside RecommendationList?
+data class RecommendedMedia (
+    val id: Int,
+    val listIndex: Int, // maybe index is not needed? could maintain list index inside object without this. unsure if it's stored 1-to-1 in sequence
+    //@Id val listId: String - should it have a unique ID in this list? or is it sufficient with index?
+    val userRating: Int?,
+    val userComment: String?,
+    val title: String,
+    val originalTitle: String?,
+    val description: String?,
+    val mediaType: MediaType,
+    val posterPath: String?, // get from backend
+    val imdbPath: String?, // backend
+    val genres: List<String>
+)
+
+enum class MediaType {tv, movie, person}
